f97120ef1b6ce91e1e179fd747cf0fa38376ce0a
KevinDonkers <kevindonkers1@hotmail.com>
2015-07-21 14:30:49 -0400

    added entity framework and simple recipies display page

---
+++ Assignment2/Admin/recipes.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="recipes.aspx.cs" Inherits="Assignment2.Admin.recipes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Recipes</h1>
+    <a href="recipe.aspx">Add A Recipe</a>
+    <div>
+        <label for="ddlPageSize">Records Per Page:</label>
+        <asp:DropDownList ID="ddlPageSize" runat="server" OnSelectedIndexChanged="ddlPageSize_SelectedIndexChanged" AutoPostBack="true">
+            <asp:ListItem Value="3" Text="3" />
+            <asp:ListItem Value="5" Text="5" />
+            <asp:ListItem Value="10" Text="10" />
+        </asp:DropDownList>
+    </div>
+    <asp:GridView ID="grdRecipes" runat="server" CssClass="table table-hover"
+        AutoGenerateColumns="false" OnRowDeleting="grdRecipes_RowDeleting" 
+        DataKeyNames="recipe_id" AllowPaging="true" OnPageIndexChanging="grdRecipes_PageIndexChanging" PageSize="3"
+        OnSorting="grdRecipes_Sorting" AllowSorting="true" OnRowDataBound="grdRecipes_RowDataBound">
+        <Columns>
+            <asp:BoundField DataField="recipe_id" SortExpression="recipe_id" HeaderText="Recipe " />
+            <asp:BoundField DataField="recipe_name" SortExpression="recipe_name" HeaderText="Recipe Name" />
+            <asp:HyperLinkField HeaderText="Edit" NavigateUrl="~/recipe.aspx" Text="Edit"
+                DataNavigateUrlFields="recipe_id" DataNavigateUrlFormatString="recipe.aspx?recipe_id={0}" />
+            <asp:CommandField HeaderText="Delete" DeleteText="Delete" ShowDeleteButton="true" />
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Assignment2/Admin/recipes.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//reference our entity framework models
+using Assignment2.Models;
+using System.Web.ModelBinding;
+
+using System.Linq.Dynamic;
+
+namespace Assignment2.Admin
+{
+    public partial class recipes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //fill the grid
+            if (!IsPostBack)
+            {
+                Session["SortDirection"] = "ASC";
+                Session["SortColumn"] = "recipe_id";
+                GetRecipes();
+            }
+        }
+
+        protected void GetRecipes()
+        {
+            //connect using our connection string from web.config and EF context class
+            using (DefaultConnectionEF conn = new DefaultConnectionEF())
+            {
+
+                //use link to query the Departments model
+                var recipes = from r in conn.Recipes
+                              select r;
+
+                //append the current direction to the sort column
+                String sort = Session["SortColumn"].ToString() + " " + Session["SortDirection"].ToString();
+                grdRecipes.DataSource = recipes.AsQueryable().OrderBy(sort).ToList();
+                grdRecipes.DataBind();
+            }
+        }
+
+        protected void ddlPageSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set the page size and refresh
+            grdRecipes.PageSize = Convert.ToInt32(ddlPageSize.SelectedValue);
+            GetRecipes();
+        }
+
+        protected void grdRecipes_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //connect
+            using (DefaultConnectionEF conn = new DefaultConnectionEF())
+            {
+                //get the selected recipe_id
+                Int32 Recipe_id = Convert.ToInt32(grdRecipes.DataKeys[e.RowIndex].Values["recipe_id"]);
+
+                var recipe = (from r in conn.Recipes
+                              where r.recipe_id == Recipe_id
+                              select r).FirstOrDefault();
+
+                //delete
+                conn.Recipes.Remove(recipe);
+                conn.SaveChanges();
+
+                //update the grid
+                GetRecipes();
+
+            }
+        }
+
+        protected void grdRecipes_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            //set the global sort cloumn to the column clicked on by the user
+            Session["SortColumn"] = e.SortExpression;
+            GetRecipes();
+
+            //toggle the direction
+            if (Session["SortDirection"] == "ASC")
+            {
+                Session["SortDirection"] = "DESC";
+            }
+            else
+            {
+                Session["SortDirection"] = "ASC";
+            }
+        }
+
+        protected void grdRecipes_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (IsPostBack)
+            {
+                if (e.Row.RowType == DataControlRowType.Header)
+                {
+                    Image SortImage = new Image();
+
+                    for (int i = 0; i < grdRecipes.Columns.Count; i++)
+                    {
+                        if (grdRecipes.Columns[i].SortExpression == Session["SortColumn"].ToString())
+                        {
+                            if (Session["SortDirection"].ToString() == "DESC")
+                            {
+                                SortImage.ImageUrl = "/images/desc.jpg";
+                                SortImage.AlternateText = "Sort Decending";
+                            }
+                            else
+                            {
+                                SortImage.ImageUrl = "/images/asc.jpg";
+                                SortImage.AlternateText = "Sort Ascending";
+                            }
+
+                            e.Row.Cells[i].Controls.Add(SortImage);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void grdRecipes_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //set the page index and refresh the grid
+            grdRecipes.PageIndex = e.NewPageIndex;
+            GetRecipes();
+        }
+    }
+}
