feda7052ec08e6c96c4a8bf87aed85e5cad67a02
KevinDonkers <kevindonkers1@hotmail.com>
2015-07-21 18:56:15 -0400

    added error handling to global.asax.cs

--- Assignment2/Global.asax.cs
+++ Assignment2/Global.asax.cs
@@ -12,5 +12,27 @@ namespace Assignment2
         protected void Application_Start(object sender, EventArgs e)
         {
         }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // An error has occured on a .Net page.
+            var serverError = Server.GetLastError() as HttpException;
+
+            if (null != serverError)
+            {
+                int errorCode = serverError.GetHttpCode();
+
+                if (404 == errorCode)
+                {
+                    Server.ClearError();
+                    Server.Transfer("~/404.aspx");
+                }
+            }
+
+
+            // Clear the error from the server
+            Server.ClearError();
+            Server.Transfer("~/error.aspx");
+        }
     }
 }
